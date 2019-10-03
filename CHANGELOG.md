fa7c720f5acdd7a66fb8e7af1cb5fd7e75cc0281
Pablo Marti <pablo.marti.dev@gmail.com>
2019-10-03 16:44:31 -0600

    Integrate with CircleCI

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+### 0.2.0
+
+- Integrate CircleCI
+
+## 0.1.0
+
+- Initial Release
--- lib/sidekiq/cron/web/admin/version.rb
+++ lib/sidekiq/cron/web/admin/version.rb
@@ -2,7 +2,7 @@ module Sidekiq
   module Cron
     module Web
       module Admin
-        VERSION = "0.1.0"
+        VERSION = "0.2.0"
       end
     end
   end
